bbabe1837efc6545e30a6b7aabcbf4b3f6a84fd9
andrewKarachun0304 <andrusha00765@gmail.com>
2020-10-09 18:56:48 +0300

    add activitys

---
+++ app/src/main/java/com/andrewkarachun0304/notesapp/CreateNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.andrewkarachun0304.notesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_note)
+    }
+}
---
+++ app/src/main/java/com/andrewkarachun0304/notesapp/GaleryActivity.kt
@@ -0,0 +1,11 @@
+package com.andrewkarachun0304.notesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GaleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_galery)
+    }
+}
