2a402738dd6fd5f51cd9979da038044ed83b3de3
Roman Ulasenka <chicony83@gmail.comgit config --listgit config --global user.email chicony83@gmail.com>
2020-10-09 19:12:25 +0300

    add Button in activity_main.xml

--- app/src/main/java/com/andrewkarachun0304/notesapp/MainActivity.kt
+++ app/src/main/java/com/andrewkarachun0304/notesapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.andrewkarachun0304.notesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        add_note_button.setOnClickListener{
+            val intent = Intent(this,CreateNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
