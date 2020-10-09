fc0c97f441436afac514005edca0403d83ee9532
Roman Ulasenka <chicony83@gmail.comgit config --listgit config --global user.email chicony83@gmail.com>
2020-10-09 20:17:25 +0300

    add in activity_main.xml RecyclerView add item_notes.xml add NotesAdapter.kt add Notes.kt

--- app/src/main/java/com/andrewkarachun0304/notesapp/MainActivity.kt
+++ app/src/main/java/com/andrewkarachun0304/notesapp/MainActivity.kt
@@ -3,12 +3,22 @@ package com.andrewkarachun0304.notesapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.andrewkarachun0304.notesapp.adapter.NotesAdapter
+import com.andrewkarachun0304.notesapp.dto.notesDataBase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        notesDataBase.filingTheBase()
+        val adapter = NotesAdapter(notesDataBase.notesDB)
+
+        notesRecycler.adapter = adapter
+        notesRecycler.layoutManager = LinearLayoutManager(this)
+        notesRecycler.hasFixedSize()
 
         add_note_button.setOnClickListener{
             val intent = Intent(this,CreateNoteActivity::class.java)
